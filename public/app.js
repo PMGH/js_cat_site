510b7568b4229e153bf3c0a6787a30d6d5d69906
Peter McCready <petermccready2@gmail.com>
2017-12-04 16:53:52 +0000

    Adds Cat class and fixes display errors

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 var app = function(){
-  var cat = {
-    name: "Elf",
-    fav_food: "Salmon",
-    image: ""
+  var Cat = function(name, fav_food, image){
+    this.name = name;
+    this.fav_food = fav_food;
+    this.image = image;
   }
 
   var createUnorderedList = function(){
@@ -12,30 +12,42 @@ var app = function(){
   }
 
   var createListElements = function(){
+    var listElements = [];
     for (var i = 0; i < 3; i++){
-      var listElement = document.createElement('li');
+      listElements.push(document.createElement('li'));
     }
     return listElements;
   }
 
-  var configureListElements = function(cat, listElements){
-    listElements[0].innerText = "Name: " + cat.name;
-    listElements[1].innerText = "Favourite Food: " + cat.fav_food;
-  }
-
   var createImage = function(cat){
     var image = document.createElement('img');
-    var width = "500";
-    image.innerHTML = "<img width = " + " src=" + cat.image + " />";
+    image.width = "500";
+    image.src = cat.image;
     return image;
   }
 
   var appendElements = function(cat, unorderedList, listElements){
-    listElements[2].appendChild(cat.image);
-    unorderedList.appendChild(listElements);
+    listElements[0].innerText = "Name: " + cat.name;
+    listElements[1].innerText = "Favourite Food: " + cat.fav_food;
+    var catImage = createImage(cat);
+    listElements[2].appendChild(catImage);
+
+    for (var element of listElements){
+      unorderedList.appendChild(element);
+    }
+
     var section = document.querySelector('#cats');
     section.appendChild(unorderedList);
   }
+
+  var createCat = function(cat){
+    var unorderedList = createUnorderedList();
+    var listElements = createListElements();
+    appendElements(cat, unorderedList, listElements);
+  }
+
+  var cat = new Cat("Genie", "Salmon", "https://static.pexels.com/photos/20787/pexels-photo.jpg");
+  createCat(cat);
 
 }
 
