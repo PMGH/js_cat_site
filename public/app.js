12c72f624fb868072fa5307179bfe2b76e371e68
Peter McCready <petermccready2@gmail.com>
2017-12-04 16:07:23 +0000

    Adds createUnorderedList() function

--- public/app.js
+++ public/app.js
@@ -1,1 +1,10 @@
+var app = function(){
+  var createUnorderedList = function(){
+    var unorderedList = document.createElement('ul');
+    unorderedList.classList.add("cat");
+    return unorderedList;
+  }
 
+}
+
+window.onload = app;
