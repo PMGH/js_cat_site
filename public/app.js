31b2c835fb000d7769c79e9724ec77408647ce08
Peter McCready <petermccready2@gmail.com>
2017-12-04 16:26:55 +0000

    Adds configureListElements() function

--- public/app.js
+++ public/app.js
@@ -1,4 +1,10 @@
 var app = function(){
+  var cat = {
+    name: "Elf",
+    fav_food: "Salmon",
+    image: ""
+  }
+
   var createUnorderedList = function(){
     var unorderedList = document.createElement('ul');
     unorderedList.classList.add("cat");
@@ -12,21 +18,24 @@ var app = function(){
     return listElements;
   }
 
-  var createImage = function(src){
+  var configureListElements = function(cat, listElements){
+    listElements[0].innerText = "Name: " + cat.name;
+    listElements[1].innerText = "Favourite Food: " + cat.fav_food;
+  }
+
+  var createImage = function(cat){
     var image = document.createElement('img');
     var width = "500";
-    image.innerHTML = "<img width = " + " src=" + src + " />";
+    image.innerHTML = "<img width = " + " src=" + cat.image + " />";
     return image;
   }
 
-  var appendElements = function(unorderedList, listElements, image){
-    listElements[2].appendChild(image);
+  var appendElements = function(cat, unorderedList, listElements){
+    listElements[2].appendChild(cat.image);
     unorderedList.appendChild(listElements);
     var section = document.querySelector('#cats');
     section.appendChild(unorderedList);
   }
-
-  
 
 }
 
