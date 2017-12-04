6a8055de58fe9ac00e708a757529b50996f53895
Peter McCready <petermccready2@gmail.com>
2017-12-04 20:18:20 +0000

    Adds additional cats to cats array

--- public/app.js
+++ public/app.js
@@ -21,7 +21,6 @@ var app = function(){
 
   var createImage = function(cat){
     var image = document.createElement('img');
-    image.width = "500";
     image.src = cat.image;
     return image;
   }
@@ -49,7 +48,12 @@ var app = function(){
   var cats = [
     {name: "Snap", fav_food: "Tuna", image: "https://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg"},
     {name: "Genie", fav_food: "Salmon", image: "https://static.pexels.com/photos/20787/pexels-photo.jpg"},
-    {name: "Harry", fav_food: "Strawberries", image: "https://d1wn0q81ehzw6k.cloudfront.net/additional/thul/media/6f97ebf075cf8075?w=400&h=400"}
+    {name: "Harry", fav_food: "Strawberries", image: "https://d1wn0q81ehzw6k.cloudfront.net/additional/thul/media/6f97ebf075cf8075?w=400&h=400"},
+    {name: "Ricky", fav_food: "Tuna", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbCPUa6k9Q2FVqNamuLkn8ZqfqhVBkAcZGr56uiICJR6mqn3XV"},
+    {name: "Sammy", fav_food: "Salmon", image: "http://cdn7.viralscape.com/wp-content/uploads/2015/07/Donald-Trump-Cat-17.jpg"},
+    {name: "Cooper", fav_food: "Tuna", image: "http://www.icenews.is/wp-content/uploads/2015/02/animals-1-700x700.jpg"},
+    {name: "Ruby", fav_food: "Haddock", image: "http://www.dewintonvet.com/wp-content/uploads/2015/02/shutterstock_9627004.jpg"},
+    {name: "Nala", fav_food: "Pigeon", image: "https://static.boredpanda.com/blog/wp-content/uploads/2017/06/funny-cats-catnip-32-59412c2b1c09a__605.jpg"}
   ]
 
   for (var i = 0; i < cats.length; i++){
